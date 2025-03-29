86cbc2cc8a737ee56a1d68323ebec51abd776056
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-03-29 15:18:02 +0500

    Update Pratice.py
    Random number gentared using random.randiant()

--- Pratice.py
+++ Pratice.py
@@ -1,12 +1,15 @@
-import math
+import numpy as np
 def magnitude(x, y):
-    return math.sqrt(x*x + y*y)
+    return np.sqrt(x*x + y*y)
 
 def unit(x,y):
     mag = magnitude(x,y)
     return (x/mag, y/mag)
 
-x, y = 5,6
+x, y = np.random.randint(100),np.random.randint(100) #using random numpy's libary
 
+
+print(x)
+print(y)
 print("magnitude:", magnitude(x,y))
 print("unit:", unit(x, y))
