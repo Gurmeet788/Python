1a5d525c24933ad46ab04255a1bdf9d13f2aad7f
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-03-29 19:21:24 +0500

    Update Pratice.py
    Udemy first leture

--- Pratice.py
+++ Pratice.py
@@ -8,8 +8,17 @@ def unit(x,y):
 
 x, y = np.random.randint(100),np.random.randint(100) #using random numpy's libary
 
+z = np.random.rand() #rand function give the random value of 0 to 1 postive if we use randn mean both neg and pos
+arr = np.random.randint(5,10,size = 2) #the first two number are range and 3rd is size of array
+arr2 = np.random.randint(1,100, size = (3,2)) #2d array
 
-print(x)
-print(y)
-print("magnitude:", magnitude(x,y))
-print("unit:", unit(x, y))
+array = np.array([1,2,3,4,5])
+print(np.random.choice(array, 5)) #it will print 5 size random array  but the number are choise from array [1,2,3,4,5]
+# print(array)
+# print(arr)
+# print(arr2)
+# print(z)
+# print(x)
+# print(y)
+# print("magnitude:", magnitude(x,y))
+# print("unit:", unit(x, y))
