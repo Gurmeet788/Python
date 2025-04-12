ee591c3f27437401b1c4a28e96cfefb5b4c07dac
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-04-12 23:59:37 +0500

    Shape Function

---
+++ Array_Shape.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+Array_1d = np.array([1,2,3,4])
+Array_2d = np.array([[1,2,3,4],[5,6,7,8]])
+Array_3d = np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
+
+print(Array_1d.shape)
+print(Array_2d.shape) # shape function tell about the shape of array like how many row and cloume are in and the dimenmision of array
+
+Arr = np.array([1,5,7],ndmin=5) #ndmin function make the dimention
+print(Arr.shape)
