e771ffc2c33ff1f8efa27722fede770e672147be
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-04-15 21:08:24 +0500

    probplitie

---
+++ Random_Probilty.py
@@ -0,0 +1,14 @@
+import numpy as np
+prob = np.array([0.1,0.3,0.2,0.4])
+array = np.array([1,2,3,4])
+x = np.random.choice(array, p=prob,size=(5),replace=True)
+
+print(x)
+
+#this whole program is about probplitie mean the computer chiose random number but we give tham the the problity of choise mean above program we say number have 10% chase to come and 2 have 30% ok we set chase.
+
+#Replace mean the dublicate is allow or not 
+#Size mean how many number you want 
+# p = probity
+
+#Note: The sum of problity set is 1 not more or less than 1.
