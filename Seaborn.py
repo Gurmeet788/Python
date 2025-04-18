1d9bdde6bbc11c5167b1b6c1531aa41283563cd4
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-04-18 23:31:14 +0500

    Seaborn

--- Seaborn.py
+++ Seaborn.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = {'Day': ['Mon', 'Tue', 'Wed'], 'Sales': [200, 150, 300]}
+data = {'Day': ['Mon', 'Tue', 'Wed'], 'Sales': [200, 150, 300]} #Day and slaes(Keys)and the array in both are values are the dictinoy in python and like hash map in java 
 
 sns.barplot(x='Day', y='Sales', data=data)
 sns.boxenplot(x='Day', y='Sales', data=data)
@@ -11,4 +11,4 @@ plt.show()
 #seaborn is a libary that is use for data visulaztion with different graph 
 
 #plt is help us to print the graph.
-#seaborn change the data into graph from.
+#seaborn change the data into graph from.u
