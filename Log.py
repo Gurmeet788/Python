b77388b7dd1f9d7f19040973535d7314d6b838e9
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-04-28 01:38:08 +0500

    Log function in numpy

---
+++ Log.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array = np.array([1,np.e,np.e**2,10]) #The symbol e represents Euler's number, a mathematical constant that is approximately equal to e≈2.71828
+
+print(np.log(array)) #log mean natural log
+
+array1 = np.array([1,2,8,12])
+
+print(np.log2(array1)) #log base 2 
+
+array2 = np.array([10,20,30])
+
+print(np.log10(array2)) #log base 10
